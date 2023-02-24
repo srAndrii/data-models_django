3f9292cc689343192ef52c3ce60f66fe1bc0aaa6
srAndrii <55akif5548@gmail.com>
2023-02-24 10:09:09 +0200

    Using the Slug & Updating Field Options

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:id>', views.book_details, name='book-detail')
+    path('<slug:slug>', views.book_details, name='book-detail')
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -12,12 +12,12 @@ def index(request):
         'books':books
     })
 
-def book_details(request, id):
+def book_details(request, slug):
     # try:
     #     book =Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_details.html', {
         "title":book.title,
         'author':book.author,
