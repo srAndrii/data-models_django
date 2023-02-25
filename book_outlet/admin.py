fa7509200c06f1e79ccf1828a0814ccd363fd0c4
srAndrii <55akif5548@gmail.com>
2023-02-25 20:04:43 +0200

    Adding a one-to-one Relation

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     list_filter=('author', 'rating',)
-    list_display=('title','author',)
+    list_display=('title','author')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
